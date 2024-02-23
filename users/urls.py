9df1b909d08fd7ae72fd981833bd8af2f364ebc9
Daniel-anti <ddn4fw@virginia.edu>
2024-02-23 12:37:59 -0500

    Refactoring files

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile", views.profile, name = "login"),
+    path("profile", views.profile, name ="login"),
     path("logout", views.logout_view)
 ]
