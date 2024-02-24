d94311821fd47c17ab5aa4bf6efa02cc5f39885b
Daniel-anti <ddn4fw@virginia.edu>
2024-02-24 15:39:37 -0500

    Added a test view. Thinking the error page is causing the heroku crash

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("profile", views.profile, name ="login"),
-    path("logout", views.logout_view)
+    path("logout", views.logout_view),
+    path("",views.test)
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 
 def profile(request):
     if request.user.groups.filter(name="Site Admin").exists():
@@ -14,3 +14,5 @@ def logout_view(request):
     logout(request)
     return redirect("/whistleblower/")
 
+def test(request):
+    return HttpResponse ("<h1> Is this working</h1>")
