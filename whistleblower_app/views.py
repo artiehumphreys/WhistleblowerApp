3b1515bb28c4ab24941fc0f71b587b6479ed3405
Cole Edwards <wsy3ux@virginia.edu>
2024-03-14 16:17:02 -0400

    Trying to get files to upload to s3, app will not run on this branch rn

--- whistleblower_app/views.py
+++ whistleblower_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import UploadFileForm
 
 
 def index(request):
     return render(request, "whistleblower_app/index.html")
 
 
+def file_upload_view(request):
+    from django.shortcuts import redirect
+    from .forms import UploadFileForm
+
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success_url')  # Redirect to a new URL
+    else:
+        form = UploadFileForm()
+    return render(request, 'your_template.html', {'form': form})
