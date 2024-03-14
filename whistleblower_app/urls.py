1c87c247b43eaf83edf55b17fefc01f14c83e6c5
Cole Edwards <wsy3ux@virginia.edu>
2024-03-14 19:30:29 -0400

    Still working on submit button and connecting it to s3

--- whistleblower_app/urls.py
+++ whistleblower_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
 from . import views
+from .views import file_upload_view
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('accounts/', include('allauth.urls')),
+    path('upload/', file_upload_view, name='upload'),
+
 ]
