9e75bc403c7c8de3bf11420d1441e74285af88f9
artiehumphreys <artieh2003@gmail.com>
2024-03-20 19:45:45 -0400

    Fixed S3. Working on site admin page

--- whistleblower_app/views.py
+++ whistleblower_app/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 from .forms import UploadFileForm
 from .models import UploadedFile
+import boto3
+from django.conf import settings
+
 
 
 def index(request):
@@ -20,5 +23,11 @@ def file_upload_view(request):
     return render(request, "whistleblower_app/file_upload.html", {'form': form})
 
 def list_files(request):
-    files = UploadedFile.objects.all()
+    s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+    response = s3.list_objects_v2(Bucket='b29-whistleblower')
+
+    files = []
+    if 'Contents' in response:
+        files = [{'name': item['Key']} for item in response['Contents']]
+
     return render(request, "users/templates/siteadmin.html", {'files': files})
