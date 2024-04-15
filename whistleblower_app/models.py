34a3f7a1cd5a7827ec9074152303f98a2d98fa78
artiehumphreys <artieh2003@gmail.com>
2024-04-15 18:13:32 -0400

    Added uuid for submissions to ensure uniqueness

--- whistleblower_app/models.py
+++ whistleblower_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.conf import settings
+import uuid
+
 
 class Submission(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.CharField(max_length=255)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
