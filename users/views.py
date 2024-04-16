59df6ec50123b38ce7c23dc16297ecb5a926246c
artiehumphreys <artieh2003@gmail.com>
2024-04-15 22:56:05 -0400

    title and submission id now successfully transfer between requests

--- users/views.py
+++ users/views.py
@@ -25,10 +25,9 @@ def profile(request):
             url = str(file_key).replace(' ', '_').replace('(', '').replace(')', '')
             metadata_response = s3.head_object(Bucket='b29-whistleblower', Key=file_key)
             metadata = metadata_response.get('Metadata', {})
-            print(metadata)
             submission_id = metadata.get('submission_id', "Old Files")
             if submission_id != None and request.user.username == metadata.get('username'):
-                submissions[submission_id].append({
+                submissions[(submission_id, metadata.get('title'))].append({
                     'url': url,
                     'name': metadata.get('title', file_key),
                     'username': metadata.get('username', 'No User Data Available'),
@@ -37,7 +36,7 @@ def profile(request):
                     'note': metadata.get('note', '')
                 })
             elif (is_site_admin and submission_id != None):
-                submissions[submission_id].append({
+                submissions[(submission_id, metadata.get('title'))].append({
                     'url': url,
                     'name': metadata.get('title', file_key),
                     'username': metadata.get('username', 'No User Data Available'),
@@ -46,10 +45,11 @@ def profile(request):
                     'note': metadata.get('note', '')
                 })
     if is_site_admin:
+        print(dict(submissions))
         return render(request, "users/siteadmin.html",{'submissions': dict(submissions)})
     else:
         return render(request, "users/profile.html", {'submissions': dict(submissions), 'form': UploadFileForm})
-    
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def change_file_status(request):
