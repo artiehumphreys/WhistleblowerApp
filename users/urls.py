827a3d9d2860cc0a5fa48ec804f4a7ca06539242
artiehumphreys <artieh2003@gmail.com>
2024-04-21 13:41:29 -0400

    Creating a new account and logging in works

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login_page'),
     path('create/', views.create_new_view, name='create_new'),
     path('user/', views.login_logic, name='login_logic'),
+    path('register/', views.register_view, name='register'),
     path("logout/", views.logout_view, name="logout"),
     path('delete_submission/<submission_id>', views.delete_submission, name='delete_submission'),
     path('accounts/', include('allauth.urls')),
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
@@ -23,12 +24,25 @@ def login_logic(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('profile')
+            return render(request, 'users/profile.html')
         else:
             return HttpResponse("Invalid username or password.", status=401)
     else:
-        return render(request, 'login.html')
-
+        return render(request, 'users/login.html')
+    
+@require_http_methods(["GET", "POST"])
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if User.objects.filter(username=username).exists():
+            return HttpResponse("Username already taken.", status=400)
+        
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return render(request, 'users/login.html')
+    else:
+        return render(request, 'users/newaccount.html')  
 def create_new_view(request):
     return render(request, 'users/newaccount.html')
 
